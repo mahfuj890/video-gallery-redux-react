7fc5c65ffd521e6690d1c9d693d0eae98fd5f2a0
mahfuj890 <mahfujali490@gmail.com>
2023-10-24 16:55:56 +0600

    create rtk query base

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "../features/api/apiSlice";
 
 export const store = configureStore({
-    reducer: {},
+  reducer: {
+    //get apiSlice name
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+
+  //for some reason need middleware to automatic task
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware.concat(apiSlice.middleware),
 });
