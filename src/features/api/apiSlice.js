bee5388be45cc0f33fdfbf92111cafa9068223c2
mahfuj890 <mahfujali490@gmail.com>
2024-08-26 13:50:38 +0600

    working on related videos

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -10,10 +10,23 @@ export const apiSlice = createApi({
       query: () => "/videos", //string or function(recommended) - params.objectName if you pass object
     }),
     getSingleVideo: builder.query({
-      query: (videoId) => `/videos/${videoId}`, 
+      query: (videoId) => `/videos/${videoId}`,
+    }),
+    getRelatedVideos: builder.query({
+      query: ({ id, title }) => {
+        console.log("title:", title)
+        const tags = title.split(" ");
+        const likes = tags.map((tag) => `title_like=${tag}`);
+        const queryString = `/videos?${likes.join("&")}&_limit=4`;
+        return queryString;
+      },
     }),
   }),
 });
 
 //automatic generate hooks based on endpoints names
-export const { useGetVideosQuery,useGetSingleVideoQuery } = apiSlice;
+export const {
+  useGetVideosQuery,
+  useGetSingleVideoQuery,
+  useGetRelatedVideosQuery,
+} = apiSlice;
